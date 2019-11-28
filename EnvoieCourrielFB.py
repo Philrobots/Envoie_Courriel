4c3698caa5cf921a52a77e5059ad0e4b52d83923
Philrobots <philippenicecool@hotmail.com>
2019-11-27 19:43:38 -0500

    reussite

--- EnvoieCourrielFB.py
+++ EnvoieCourrielFB.py
@@ -1,14 +1,14 @@
 import fbchat
-from getpass import getpass 
-username = str(input("Username: ")) 
-client = fbchat.Client(username, getpass()) 
-no_of_friends = int(input("Number of friends: ")) 
-for i in range(0, no_of_friends): 
-	name = str(input("Name: ")) 
-	friends = client.getUserActiveStatus(name) # a retravailler
-	friend = friends[0] 
-	msg = str(input("Message: ")) 
-	sent = client.send(friend.uid, msg) 
-	if sent: 
-		print("Message sent successfully!") 
-
+from getpass import getpass
+username = str(input("Username: "))
+client = fbchat.Client(username, getpass())
+no_of_friends = int(input("Number of friends: "))
+for i in range(0, no_of_friends):
+    name = str(input("Name: "))
+    friends = client.searchForGroups(name) # return a list of names
+    friend = friends[0]
+    msg = str(input("Message: "))
+    sent = client.sendMessage(msg, thread_id=friend.uid)
+    print(friend)
+    if sent:
+        print("Message sent successfully!")
